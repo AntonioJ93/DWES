820a2ef1dfd1af7d05c4d9e302aed41d8ab29caf
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2021-12-17 18:34:53 +0100

    +

--- tarea2.1/1_test/buscarPorCorreoYPass.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql = 'SELECT *  FROM usuario WHERE correo= :correo AND pass= :pass';
-
-    $sth = $conexion->prepare($sql);
-
-    $sth->execute(array(':correo' => $correo, ':pass' => $pass));
-    $res = $sth->fetch();
-?>
--- tarea2.1/1_test/buscarTodosTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql = 'SELECT *  FROM test';
-        $sth = $conexion->prepare($sql);
-
-        $sth->execute();
-        $listaTest = $sth->fetchAll();
-
-?>
