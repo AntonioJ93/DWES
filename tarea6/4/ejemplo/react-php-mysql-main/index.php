843db2ae2fe249311a9b09ecb31aa2bb42be9e1f
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2022-05-02 11:40:53 +0200

    .

---
+++ tarea6/4/ejemplo/react-php-mysql-main/index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+
+    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+    <!-- Cargamos nuestro componente de React. -->
+    <script src="components.js" type="text/babel"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
